fb8800a9d5c6ee182db7c20897217e62c2adf6f1
Rhea Gezelle Ramirez <54275927+rheagezelle@users.noreply.github.com>
2024-07-16 15:19:51 +0800

    feat: add validator to store method in StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,11 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
-    public function store(Request $request) {
-        $student = Student::create($request->all());
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(["error" => $validator->errors()], 422);
+        }
+
+        $validated = $validator->validated();
+        $student = Student::create($validated);
+
         return response()->json($student, 201);
     }
 }
