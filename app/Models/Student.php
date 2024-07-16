c12ddf2ecd922b1e329f4d2df6b272abae26dfc1
Rhea Gezelle Ramirez <>
2024-07-16 12:16:43 +0800

    fix: remove redundant table definition in Student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
-    protected $table = 'students';
     protected $fillable = [
         'first_name',
         'last_name',
