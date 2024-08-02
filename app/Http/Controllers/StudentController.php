f55402f361e7f19c1c05ae791ae4efa03e716030
Rhea Gezelle <54275927+rheagezelle@users.noreply.github.com>
2024-08-02 22:42:07 +0800

    feat: update StudentController.php to use the HandlesCrud trait

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,52 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\StudentResource;
-use Illuminate\Http\Request;
 use App\Models\Student;
+use App\Traits\HandlesCrud;
 
 class StudentController extends Controller
 {
-    public function index()
-    {
-        $students = Student::all();
-        return StudentResource::collection($students);
-    }
-    public function search(Request $request)
-    {
-        $query = $request->input('query');
+    use HandlesCrud;
 
-        $students = Student::where('first_name', 'like', "%{$query}%")
-            ->orWhere('last_name', 'like', "%{$query}%")
-            ->paginate();
-
-        return StudentResource::collection($students);
-    }
-    public function store(Request $request)
+    public function model()
     {
-        $validated = $request->validate([
+        return Student::class;
+    }
+    public function rules()
+    {
+        return [
             'first_name' => 'required|string|max:50',
             'last_name' => 'required|string|max:50',
-        ]);
-        $student = Student::create($validated);
-
-        return new StudentResource($student);
-    }
-    public function update(Request $request, string $id)
-    {
-        $validated = $request->validate([
-            'first_name' => 'nullable|string|max:50',
-            'last_name' => 'nullable|string|max:50',
-        ]);
-        $student = Student::findOrFail($id);
-
-        $student->update($validated);
-
-        return new StudentResource($student);
-    }
-    public function destroy(string $id)
-    {
-        $student = Student::findOrFail($id);
-        $student->delete();
+        ];
     }
 }
